39f1088dd0b899c936f65dfff8d79e2386967778
Riccardo Rambaldi <riccardo.rambaldi4@studio.unibo.it>
2024-04-06 15:51:26 +0200

    Complete school model test

--- src/test/scala/tasks/adts/SchoolModelTest.scala
+++ src/test/scala/tasks/adts/SchoolModelTest.scala
@@ -4,25 +4,32 @@ import u03.Sequences.Sequence
 import org.junit.Assert.*
 import org.junit.*
 import u03.Optionals.Optional
-import tasks.adts.SchoolModel.SchoolModule
-import tasks.adts.SchoolModel.SchoolModelADT.TeacherImpl
-import tasks.adts.SchoolModel.SchoolModelADT.school
-import tasks.adts.SchoolModel.SchoolModelADT.CourseImpl
-import tasks.adts.SchoolModel.SchoolModelADT.Teacher
+import tasks.adts.SchoolModel.SchoolModelADT.*
+import org.junit.jupiter.api.BeforeAll
 
 class SchoolModelTest:
 
+    var mySchool: School = school().addTeacher("Marco").addTeacher("Franco").addCourse("PPS").addCourse("PCD")
+
     @Test
     def addTeacherTest() =
-        assertEquals(Optional.Just(TeacherImpl("Marco", Sequence.Nil())), school().addTeacher("Marco").teacherByName("Marco"))
-        assertEquals(Optional.Empty(), school().addTeacher("Marco").teacherByName("Franco"))
+        assertEquals(Optional.Just(TeacherImpl("Marco", Sequence.Nil())), mySchool.teacherByName("Marco"))
+        assertEquals(Optional.Empty(), mySchool.teacherByName("Andrea"))
 
     @Test
     def addCourseTest() =
-        assertEquals(Optional.Just(CourseImpl("PPS")), school().addCourse("PPS").courseByName("PPS"))
-        assertEquals(Optional.Empty(), school().addCourse("PPS").courseByName("PCD"))
+        assertEquals(Optional.Just(CourseImpl("PPS")), mySchool.courseByName("PPS"))
+        assertEquals(Optional.Empty(), mySchool.courseByName("WEB"))
     
     @Test
     def setTeacherToCourseTest() =
         var teachers: Sequence[Teacher] = Sequence.Cons(TeacherImpl("Marco", Sequence.Cons(CourseImpl("PPS"), Sequence.Nil())), Sequence.Nil())
-        assertEquals(school(teachers, Sequence.Cons(CourseImpl("PPS"), Sequence.Nil())), school().setTeacherToCourse(TeacherImpl("Marco", Sequence.Nil()), CourseImpl("PPS")))
+        var newSchool: School = school(teachers, Sequence.Cons(CourseImpl("PPS"), Sequence.Nil()))
+        assertThrows(classOf[IllegalArgumentException], () => mySchool.setTeacherToCourse(TeacherImpl("Andrea", Sequence.Nil()), CourseImpl("PPS")))
+        assertEquals(newSchool.teacherByName("Marco"), mySchool.setTeacherToCourse(TeacherImpl("Marco", Sequence.Nil()), CourseImpl("PPS")).teacherByName("Marco"))
+
+    @Test
+    def getCourseOfTeacherTest() =
+        var courses: Sequence[Course] = Sequence.Cons(CourseImpl("PPS"), Sequence.Nil())
+        var teacher: Teacher = TeacherImpl("Marco", Sequence.Nil())
+        assertEquals(courses, mySchool.setTeacherToCourse(teacher, CourseImpl("PPS")).coursesOfATeacher(teacher))
