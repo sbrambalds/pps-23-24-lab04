3f3b451c512cd3f4524b4ff2f76b657251ff47bb
Riccardo Rambaldi <riccardo.rambaldi4@studio.unibo.it>
2024-04-06 15:51:00 +0200

    Minor changes to Ex5Traversable

--- src/main/scala/tasks/typeclasses/Ex5Traversable.scala
+++ src/main/scala/tasks/typeclasses/Ex5Traversable.scala
@@ -1,6 +1,9 @@
 package u04lab
 import u03.Sequences.* 
 import Sequence.*
+import u03.Optionals.*
+import u03.Lambda.And
+import u04lab.Ex5Traversable.logAll
 
 /*  Exercise 5: 
  *  - Generalise by ad-hoc polymorphism logAll, such that:
@@ -19,8 +22,38 @@ object Ex5Traversable:
 
   def log[A](a: A): Unit = println("The next element is: "+a)
 
-  def logAll[A](seq: Sequence[A]): Unit = seq match
-    case Cons(h, t) => log(h); logAll(t)
-    case _ => ()
+  trait Traversable[T[_]]:
+    def logAll[A](a: T[A]): Unit
+  
+  def logAll[A, T[A]: Traversable](a: T[A]): Unit = 
+    summon[Traversable[T]].logAll(a)
+
+object TraversableGivenInstances:
+  import Ex5Traversable.*
+
+  given Traversable[Optional] with
+    def logAll[A](a: Optional[A]): Unit = a match
+      case Optional.Just(v) => log(v)
+      case _ => ()
+
+  given Traversable[Sequence] with
+    def logAll[A](a: Sequence[A]): Unit = a match
+      case Sequence.Cons(h, t) => log(h); logAll(t)
+      case _ => ()
+
+@main def tryTraversable() =
+  import Ex5Traversable.*
+  import TraversableGivenInstances.{*, given}
+
+  logAll(Optional.Just(45))
+  logAll(Optional.Empty())
+  logAll(Sequence.Cons(10, Sequence.Cons(20, Sequence.Cons(30, Sequence.Nil()))))
+  logAll(Sequence.Nil())
+
+
+
+
+
+
 
   
